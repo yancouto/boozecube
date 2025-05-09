ef24be5ba64128061f06cd90231ef2c0d66da647
Yan Couto <yancouto@gmail.com>
2025-05-09 17:04:20 -0300

    Prompts with GPT

--- scripts/generate_prompts.py
+++ scripts/generate_prompts.py
@@ -3,10 +3,17 @@
 import ollama, re, textwrap
 from pathlib import Path
 
-MODEL = "gemma3:4b"
+GPT = True
+DIR = Path("../boozecube.mse-set/")
 
-DIR = "../boozecube.mse-set/"
-OUT = "prompts/"
+if GPT:
+    OUT = Path("prompts_gpt/")
+    from openai import OpenAI
+
+    gpt_client = OpenAI()
+else:
+    OUT = Path("prompts/")
+    gpt_client = None
 
 FILTER_OUT = [
     "casting_cost:",
@@ -17,11 +24,16 @@ FILTER_OUT = [
     "illustrator_brush:",
 ]
 
-for file in Path(DIR).iterdir():
+OUT.mkdir(parents=True, exist_ok=True)
+
+for file in DIR.iterdir():
     if file.is_file() and file.name.startswith("card "):
-        if Path(OUT, file.name).exists():
+        if (OUT / file.name).exists():
             continue
         card_text = file.read_text(encoding="utf-8")
+        if "image: i" in card_text:
+            # Skip cards with images
+            continue
 
         # Remove tags
         card_text = re.sub(r"</?[^>]+>", "", card_text)
@@ -36,13 +48,29 @@ for file in Path(DIR).iterdir():
         card_text = "card:\n" + textwrap.indent(card_text, "  ")
 
         print("Processing card: " + file.name[5:])
-
-        response = ollama.generate(
-            model=MODEL,
-            prompt="""
-Generate a prompt for the image of the Magic The Gathering card described below. The prompt should be on a single line, describing the card's art, and should be in the format of a prompt for Stable Diffusion. The art may be detailed or stylish and simple, depending on the card's characteristics.\n\n"""
-            + card_text,
-            keep_alive="30m",
-        )
-
-        Path(OUT, file.name).write_text(response["response"], encoding="utf-8")
+        if gpt_client is None:
+            response = ollama.generate(
+                model="gemma3:4b",
+                prompt="""
+    Generate a prompt for the image of the Magic The Gathering card described below. The prompt should be on a single line, describing the card's art, and should be in the format of a prompt for Stable Diffusion. The art may be detailed or stylish and simple, depending on the card's characteristics.\n\n"""
+                + card_text,
+                keep_alive="30m",
+            )
+            text = response["response"]
+        else:
+            response = gpt_client.responses.create(
+                model="gpt-4.1",
+                instructions="""
+    Generate a prompt for the image of the Magic The Gathering card described. The prompt should be on a single line, describing the card's art, and should be in the format of a prompt for ChatGPT, without introduction. The art may be detailed or stylish and simple, depending on the card's characteristics.
+        """,
+                input=card_text,
+                text={"format": {"type": "text"}},
+                reasoning={},
+                tools=[],
+                temperature=1,
+                max_output_tokens=2048,
+                top_p=1,
+                store=True,
+            )
+            text = response.output_text
+        (OUT / file.name).write_text(text, encoding="utf-8")
